5063831046739ea54190709168e827cdb4110070
deadseal001 <liwenbo4936@gmail.com>
2022-10-25 12:41:10 -0500

    ad CRUD routes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const { spreadProperty } = require('@babel/types');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql=require('mysql2');
+const inputCheck=require('./utils/inputCheck');
+
 
 //Express middleware
 app.use(express.urlencoded({extended: false}));//check function??
@@ -18,32 +21,83 @@ const db=mysql.createConnection(
     console.log('Connected to the election database')
 );
 
-// db.query(`SELECT* FROM candidates WHERE id =1`,(err, row)=>{
-//     if (err){
-//         console.log(err);
-//     }
-//     console.log(row)
-// });
+//get all candidates
+app.get('/api/candidates',(req,res)=>{
+    const sql = `SELECT * FROM candidates`;
+    db.query(sql,(err,rows)=>{
+        if(err){
+            res.status(500).json({error: err.message});
+            return;
+        } 
+        res.json({
+            message: 'success',
+            data:rows
+        });
+    });
+})
 
-// db.query(`DELETE FROM candidates WHERE ID=?`,1,(err,result)=>{
-//     if (err){
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
-
-//create a candidate
-
-const sql=`insert into candidates (id, first_name, last_name, industry_connected)
-            values (?,?,?,?)`;
-const params = [1,'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err,result)=>{
-    if (err){
-        console.log(err);
-    }
-    console.log(result);
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
 });
+
+//Delete a candidate
+app.delete('/api/candidate/:id',(req,res)=>{
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params= [req.params.id];
+    console.log(sql, params);
+    db.query(sql, params, (err,result)=>{
+        if (err) {
+            res.statusMessage(400).json({error: res.message});
+        } else if (!result.affectedRows){
+            res.json({message: 'Candidate not found'});
+        } else {
+            res.json({
+                Message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params,id
+            });
+        }
+    });
+})
+
+
+// create a candidate
+app.post('/api/candidate',({body},res)=>{
+    const errors= inputCheck(body,'first_name','last_name', 'industry_connected');
+    if(errors) {
+        res.status(400).json({error:errors});
+        return;
+    }
+    const sql=`insert into candidates (first_name, last_name, industry_connected)
+    values(?,?,?)`;
+    const params=[body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql,params,(err,result)=>{
+        if (err) {
+            res.status(400).json({error:err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data:body
+        });
+    });
+});
+
+
 
 app.get('/',(req,res)=>{
     res.json({message: 'Hello Woirld'})
@@ -53,6 +107,9 @@ app.get('/',(req,res)=>{
 app.use((req,res)=>{
     res.status(404).end();
 })
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
