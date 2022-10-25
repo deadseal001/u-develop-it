ae1d2a92f7357879485ebc19ab060af67aaff92e
deadseal001 <liwenbo4936@gmail.com>
2022-10-24 23:57:27 -0500

    mysql.createConnection

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const mysql=require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({extended: false}));//check function??
 app.use(express.json());
+
+//connect to database
+const db=mysql.createConnection(
+    {
+        host:'localhost',
+        user: "root",
+        password:'658Leiothrix.',
+        database:'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT* FROM candidates`,(err, rows)=>{
+    console.log(rows);
+})
+
 
 
 app.get('/',(req,res)=>{
